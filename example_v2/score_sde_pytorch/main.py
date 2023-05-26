11fa4aa1e0178406843ea247399c064c9fa13e8b
Yifu Lu <yifulu@gl-login1.arc-ts.umich.edu>
2023-05-25 20:50:30 -0400

    save

--- example_v2/score_sde_pytorch/main.py
+++ example_v2/score_sde_pytorch/main.py
@@ -26,7 +26,8 @@ from absl import flags
 from ml_collections.config_flags import config_flags
 import logging
 import torch
-
+print(local_rank)
+print(total_rank)
 torch.cuda.set_device(local_rank)
 torch.distributed.init_process_group(backend='nccl')
 
