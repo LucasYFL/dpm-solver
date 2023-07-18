f4f934723f9d0c0996ebd21c30886c32b1a3f912
Huijie Zhang <huijiezh@gl-login3.arc-ts.umich.edu>
2023-07-18 11:20:29 -0400

    update

--- example_v2/score_sde_pytorch/sampling.py
+++ example_v2/score_sde_pytorch/sampling.py
@@ -527,7 +527,7 @@ def get_dpm_solver_sampler(sde, shape, inverse_scaler, steps=10, eps=1e-3,
   """
   ns = NoiseScheduleVP('linear', continuous_beta_0=sde.beta_0, continuous_beta_1=sde.beta_1)
 
-  def dpm_solver_sampler(models, compare_step):
+  def dpm_solver_sampler(models, compare_step, seed=None):
     """ The DPM-Solver sampler funciton.
 
     Args:
@@ -541,7 +541,10 @@ def get_dpm_solver_sampler(sde, shape, inverse_scaler, steps=10, eps=1e-3,
         noise_pred_fns.append(get_noise_fn(sde, m, train=False, continuous=True))
       dpm_solver = DPM_Solver(noise_pred_fns, ns, compare_step = compare_step, algorithm_type=algorithm_type, correcting_x0_fn="dynamic_thresholding" if thresholding else None)
       # Initial sample
-      x = sde.prior_sampling(shape).to(device)
+      if seed is None:
+        x = sde.prior_sampling(shape).to(device)
+      else:
+        x = seed
       x = dpm_solver.sample(
         x,
         steps=steps - 1 if denoise else steps,
